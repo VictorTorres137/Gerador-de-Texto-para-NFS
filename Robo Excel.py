6414cf758dccb6e73a0658bb2404bfba3d9af91c
VictorTorres137 <vdst2011@hotmail.com>
2023-01-17 13:52:38 -0300

    03 Layout

--- Robo Excel.py
+++ Robo Excel.py
@@ -7,18 +7,33 @@ from kivy.properties import ObjectProperty
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.filechooser import FileChooserIconView, FileChooser
 
 
-class Tarefa(FloatLayout):
+class Prog(FloatLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    def mudatexto(self):
+        self.ids.bt2.text="12343434"
+        self.ids.lb1.text = "12343434"
+        self.ids.fl2.add_widget(FileChooser(pos_hint={"x": .0, "y": .0}, size_hint=[1, .8]))
+
+
+    # def procura(self):
+    #
+    #     filename = filedialog.askopenfilename(initialdir="/",
+    #                                           title="Select a File",
+    #                                           filetypes=(("Excel",
+    #                                                       ".xlsx"),
+    #                                                      ("all files",
+    #                                                       ".")))
+    #     return filename
 
 
 class Tela(App):
     def build(self):
-
-        return Tarefa()
+        return Prog()
 
 Tela().run()
 
