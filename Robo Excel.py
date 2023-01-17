4947c3ac76d7bacaf2239a67384e05a48ed0bb14
VictorTorres137 <vdst2011@hotmail.com>
2023-01-17 15:34:03 -0300

    04 Encontra caminho para o Arquivo

--- Robo Excel.py
+++ Robo Excel.py
@@ -9,16 +9,37 @@ from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.filechooser import FileChooserIconView, FileChooser
 
+# class Filechooser(FloatLayout):
+#     def select(self, *args):
+#         try: self.label.text = args[1][0]
+#         except: pass
+
 
 class Prog(FloatLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
     def mudatexto(self):
+        gt=''
+
         self.ids.bt2.text="12343434"
         self.ids.lb1.text = "12343434"
-        self.ids.fl2.add_widget(FileChooser(pos_hint={"x": .0, "y": .0}, size_hint=[1, .8]))
+        # self.ids.fl2.add_widget(FileChooser(pos_hint={"x": .0, "y": .0}, size_hint=[1, .8]))
+        self.caminho=FileChooserIconView(pos_hint={"x": .0, "y": .0}, size_hint=[1, .8])
 
+        try:
+
+            self.ids.fl2.add_widget(self.caminho)
+
+
+            print('Adicionou')
+        except:
+            self.ids.fl2.add_widget(self.caminho)
+            print(self.caminho)
+        return self.caminho
+    def mudatexto2(self):
+
+        print(self.caminho.selection)
 
     # def procura(self):
     #
