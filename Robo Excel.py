1791ea67afd1d4178c3262bf216548d68ab5dddb
Victor Torres <113647400+VictorTorres137@users.noreply.github.com>
2023-01-16 22:33:43 -0300

    Inicio Robo Leitura Excel

--- Robo Excel.py
+++ Robo Excel.py
@@ -1,1 +1,23 @@
-print("oi")
+# from pandas import *
+import pandas as pd
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+
+Gui=Builder.load_file('menu.kv')
+
+
+class AwesomeApp(App):
+    def build(self):
+        return Gui
+
+if __name__ == "__main__":
+    AwesomeApp().run()
+
+
+#
+#
+# arquivo = pd.read_excel("Eco Urbis-Container-0256-22-Sul.xlsx")
+#
+# print(arquivo)
+
