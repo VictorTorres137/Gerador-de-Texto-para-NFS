3e7b7a0cf582707b3f0d1d3f5d133af36ae3a934
VictorTorres137 <vdst2011@hotmail.com>
2023-01-17 13:00:57 -0300

    02

--- Robo Excel.py
+++ Robo Excel.py
@@ -3,16 +3,24 @@ import pandas as pd
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
 
-Gui=Builder.load_file('menu.kv')
+
+class Tarefa(FloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
 
-class AwesomeApp(App):
+
+class Tela(App):
     def build(self):
-        return Gui
 
-if __name__ == "__main__":
-    AwesomeApp().run()
+        return Tarefa()
+
+Tela().run()
 
 
 #
