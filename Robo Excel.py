1a9ca8ec184c5babf213e016c0aa9b72e381a47a
VictorTorres137 <vdst2011@hotmail.com>
2023-01-20 13:41:56 -0300

    Separa as linhas com os checkbox reconhece os imputs

--- Robo Excel.py
+++ Robo Excel.py
@@ -7,6 +7,7 @@ from kivy.properties import ObjectProperty
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.switch import Switch
+from kivy.uix.checkbox import CheckBox
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.filechooser import *
@@ -14,6 +15,7 @@ from kivy.uix.filechooser import *
 
 # from openpyxl
 procura_arquivo_parent = aviso_selecione_arquivo_parent= "None"
+lista_servicos=[]
 
 class Info():
     def __init__(self, num_orc, num_nf, cabc, desct, vencm, *serv):
@@ -30,7 +32,6 @@ class Prog(FloatLayout):
         super().__init__(**kwargs)
 
         self.bt1 = Button(text='Procure o Arquivo', size_hint=[.3, .1], pos_hint={"x": .35, "y": .9})
-        # self.bt1 = Button(text='Procure o Arquivo', size_hint=[.3, .1], pos_hint={"x": .35, "y": .9}, on_press=self.interface_procura)
         self.bt1.bind(on_press=lambda a='' : self.interface_procura())
         self.add_widget(self.bt1)
 
@@ -39,30 +40,16 @@ class Prog(FloatLayout):
 
         global procura_arquivo_parent
 
-        # self.novo = ToggleButton(text='Male', group='sex', pos_hint={"x": .35, "y": .6}, size_hint=[.3, .1])
-
         self.procura_arquivo = FileChooserListView(pos_hint={"x": .05, "y": .1}, size_hint=[.9, .3], filters=["*.xlsx"])
-
         self.btok = Button(text="OK", pos_hint={"x": .35, "y": .8}, size_hint=[.3, .1], on_press=lambda a='': self.seleciona_arquivo())
-
-        print(self.procura_arquivo.parent)
 
 
         if procura_arquivo_parent == "None":
 
-
-            # self.add_widget(self.novo)
             self.add_widget(self.btok)
             self.add_widget(self.procura_arquivo)
 
-
-
             procura_arquivo_parent = "Já exixte"
-
-
-
-
-
 
     def seleciona_arquivo(self):
 
@@ -80,6 +67,7 @@ class Prog(FloatLayout):
             self.remove_widget(self.procura_arquivo)
             if aviso_selecione_arquivo_parent != 'None':
                 self.remove_widget(self.aviso_selecione_arquivo)
+                aviso_selecione_arquivo_parent = "None"
 
             procura_arquivo_parent = "None"
 
@@ -109,24 +97,50 @@ class Prog(FloatLayout):
             print(servc[n]['valor'])
 
 
-
         infos.serv=servc
 
         print(infos.serv)
 
         self.interface_servicos()
+
+
     def interface_servicos(self):
 
-        global infos
+        global infos, lista_servicos
 
-        for n in range (0, len(infos.serv)):
-            # print(infos.serv[n]['servico'], end='==')
-            # print(infos.serv[n]['valor'])
-            self.novo = Label(text=infos.serv[n]['servico'], pos_hint={"x": .1, "y": .7-(n/20)}, size_hint=[.3, .05])
-            self.novo2 = Switch(pos_hint={"x": .3, "y": .7 - (n / 20)}, size_hint=[.3, .05])
-            # self.novo3 = Switch(text='123')
-            self.add_widget(self.novo)
-            self.add_widget(self.novo2)
+
+        #
+        # for k in range(0, len(infos.serv)):
+        #     globals()[f"self.novo{str(k)}"] = Label()
+        #     globals()[f"self.novo2{str(k)}"] = CheckBox()
+        #
+        #     # lista_servicos.append(globals()[f"self.novo{str(k)}"])
+        #     lista_servicos.append(globals()[f"self.novo2{str(k)}"])
+
+
+
+        for n in range(0, len(infos.serv)):
+
+            globals()[f"self.novo{str(n)}"] = Label(text=infos.serv[n]['servico'], pos_hint={"x": .1, "y": .7-(n/20)}, size_hint=[.3, .05])
+            # globals()[f"self.novo2{str(n)}"] = CheckBox(pos_hint={"x": .3, "y": .7 - (n / 20)}, size_hint=[.3, .05], active=lambda a=str(n): self.estado_switch(a))
+            globals()[f"self.novo2{str(n)}"] = CheckBox(pos_hint={"x": .3, "y": .7 - (n / 20)}, size_hint=[.3, .05])
+            lista_servicos.append({'Label' : globals()[f"self.novo{str(n)}"], 'Checkbox': globals()[f"self.novo2{str(n)}"]})
+
+
+            self.add_widget(globals()[f"self.novo{str(n)}"])
+            self.add_widget(globals()[f"self.novo2{str(n)}"])
+
+
+        self.btok2=Button(text="OK2", pos_hint={"x": .35, "y": .8}, size_hint=[.3, .1], on_press=lambda a='': self.estado_switch())
+        self.add_widget(self.btok2)
+
+    def estado_switch(self):
+
+        global lista_servicos
+
+        for j in lista_servicos:
+            print(j["Checkbox"].active)
+
 
 class Tela(App):
     def build(self):
